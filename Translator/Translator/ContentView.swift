337a8965fa79f5026a3084b9e20f7326835b6702
glbrom <golbrm@gmail.com>
2025-02-11 19:14:06 +0200

    create empty project

---
+++ Translator/Translator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Translator
+//
+//  Created by Roman Golub on 11.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Translator/Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Roman Golub on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
